4ecde6fe76461020ecf1ac63d5f8466e6bd6742a
Rashid Aziz <md.r.a.n.786@gmail.com>
2023-04-25 16:38:42 +0530

    updated

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -5,7 +5,6 @@ const NotesItem = (props) => {
     const context = useContext(notescontext);
     const {deleteNote} = context;
     const {note, updateNote} = props;
-
     return (
         <div className='col-md-3'>
             <div className="card my-3 mx-1">
@@ -14,7 +13,7 @@ const NotesItem = (props) => {
                     <p className="card-text">Course - {note.description}</p>
                     <p className="card-text">PT1 - {note.pt1}</p>
                     <p className="card-text">PT2 - {note.pt2}</p>
-                    <p className="card-text">Avg - {note.avg}</p>
+                    <p className="card-text">Avg - {(note.pt1 + note.pt2)/2}</p>
                     <i className="fa-sharp fa-solid fa-trash mx-3" onClick={() => {deleteNote(note._id)}}></i>
                     <i className="fa-solid fa-pen-to-square mx-3" onClick={()=>{updateNote(note)}}></i>
                 </div>
